9877621df12ca226edfa688b62f2f866efa824e8
KING <abayemiracle@gmail.com>
2020-12-27 16:28:46 -0800

    env added

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 
 
 app.use(express.json());
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
 
-const port = 5000;
 
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
